63801c5757e7fbc64c47da66eae0c74a6ca45f82
DoctorWil <wilian.lima.santoss@gmail.com>
2022-04-16 16:08:40 -0300

    golem de abelhas feitas
    golem de abelhas feitas

--- src/main/java/br/com/ohanacraft/biotech/resource/Bee.java
+++ src/main/java/br/com/ohanacraft/biotech/resource/Bee.java
@@ -9,14 +9,50 @@ import org.bukkit.inventory.ItemStack;
 
 public class Bee {
 
-    public static final SlimefunItemStack CUSTOM_OHANA_SALADA_DE_FRUTA =
-            new SlimefunItemStack("CUSTOM_OHANA_SALADA_DE_FRUTA",
-                    "f1a3727b9be72f6ac65ed9d303a1776f4cadfac00db38882e241a9ddfcaa3a56",
-                    "&cSalada de Fruta", "");
+    public static final SlimefunItemStack SIMPLE_BEE =
+            new SlimefunItemStack("SIMPLE_BEE",
+                    "259001a851bb1b9e9c05de5d5c68b1ea0dc8bd86babf188e0aded8f912c07d0d",
+                    "&eAbelha &fComum", "");
+
+    public static final SlimefunItemStack INTELLIGENCE_BEE =
+            new SlimefunItemStack("INTELLIGENCE_BEE",
+                    "1445c3def689427fd8df8cad824c0a6b553d4f847eb03705196a69c7eed946af",
+                    "&eAbelha &fInteligente", "");
+
+    public static final SlimefunItemStack LOCK_BEE =
+            new SlimefunItemStack("LOCK_BEE",
+                    "259001a851bb1b9e9c05de5d5c68b1ea0dc8bd86babf188e0aded8f912c07d0d",
+                    "&eAbelha &fSortuda", "");
+
+    public static final SlimefunItemStack BERSERK_BEE =
+            new SlimefunItemStack("BERSERK_BEE",
+                    "d8916ea1ffb39f13dbbf4e62449dd2cbd5d2812f414e97608af0609e139d6115",
+                    "&eAbelha &fBerserk", "");
+
+    public static final SlimefunItemStack EFFICIENTY_BEE =
+            new SlimefunItemStack("EFFICIENTY_BEE",
+                    "fb7a761d715d2c3c73a7d01411e61f4237e4f7633cc9f4db2cf05f66d2030a17",
+                    "&eAbelha &fEficiente", "");
+
+    public static final SlimefunItemStack CLONING_BEE =
+            new SlimefunItemStack("CLONING_BEE",
+                    "2ea2ba84c8c91dcb28af56a36dd49a1e51c3557e853f5d3777877758737b5557",
+                    "&eAbelha &fClonadora", "");
+
+    public static final SlimefunItemStack ACCELERATION_BEE =
+            new SlimefunItemStack("ACCELERATION_BEE",
+                    "ACCELERATION_BEE",
+                    "&eAbelha &fAceleradora", "");
 
     public static void setup(BioTech plugin) {
 
-        preSetup(plugin, Bee.CUSTOM_OHANA_SALADA_DE_FRUTA);
+        preSetup(plugin, Bee.SIMPLE_BEE);
+        preSetup(plugin, Bee.INTELLIGENCE_BEE);
+        preSetup(plugin, Bee.LOCK_BEE);
+        preSetup(plugin, Bee.BERSERK_BEE);
+        preSetup(plugin, Bee.EFFICIENTY_BEE);
+        preSetup(plugin, Bee.CLONING_BEE);
+        preSetup(plugin, Bee.ACCELERATION_BEE);
 
     }
 
--- src/main/java/br/com/ohanacraft/biotech/resource/Golem.java
+++ src/main/java/br/com/ohanacraft/biotech/resource/Golem.java
@@ -9,14 +9,50 @@ import org.bukkit.inventory.ItemStack;
 
 public class Golem {
 
-    public static final SlimefunItemStack CUSTOM_OHANA_SALADA_DE_FRUTA =
-            new SlimefunItemStack("CUSTOM_OHANA_SALADA_DE_FRUTA",
-                    "f1a3727b9be72f6ac65ed9d303a1776f4cadfac00db38882e241a9ddfcaa3a56",
-                    "&cSalada de Fruta", "");
+    public static final SlimefunItemStack SIMPLE_GOLEM =
+            new SlimefunItemStack("SIMPLE_GOLEM",
+                    "e13f34227283796bc017244cb46557d64bd562fa9dab0e12af5d23ad699cf697",
+                    "&eGolem &fComum", "");
+
+    public static final SlimefunItemStack INTELLIGENCE_GOLEM =
+            new SlimefunItemStack("INTELLIGENCE_GOLEM",
+                    "562f0e9a738265c75bdd92b9e2e02377fec9f3b027086a0dec273bbdf6798be",
+                    "&eGolem &fInteligente", "");
+
+    public static final SlimefunItemStack LOCK_GOLEM =
+            new SlimefunItemStack("LOCK_GOLEM",
+                    "ef6e7a9c941e71bbfeb9ccdf91119ed7868bef05e661b1e14d3e34a9b17088d9",
+                    "&eGolem &fSortuda", "");
+
+    public static final SlimefunItemStack BERSERK_GOLEM =
+            new SlimefunItemStack("BERSERK_GOLEM",
+                    "ca14326aeac97f96189bb9969e7b955ec650d37b7c9790356ff09f32010f1ae0",
+                    "&eGolem &fBerserk", "");
+
+    public static final SlimefunItemStack EFFICIENTY_GOLEM =
+            new SlimefunItemStack("EFFICIENTY_GOLEM",
+                    "6d351a4ef45749565514d22f48e9695005a582b9ddbbbc444647ff0b056f4ffa",
+                    "&eGolem &fEficiente", "");
+
+    public static final SlimefunItemStack CLONING_GOLEM =
+            new SlimefunItemStack("CLONING_GOLEM",
+                    "ec4a74ac2d0ceff89157ad16121b6bcb2b5e5e694b460a27d3b9944f368c17ce",
+                    "&eGolem &fClonadora", "");
+
+    public static final SlimefunItemStack ACCELERATION_GOLEM =
+            new SlimefunItemStack("ACCELERATION_GOLEM",
+                    "1bfb3534baf81dc3f82feffbcd1c9ae33fac7dcc8291d2ee43d819d5a9b61",
+                    "&eGolem &fAceleradora", "");
 
     public static void setup(BioTech plugin) {
 
-        preSetup(plugin, Golem.CUSTOM_OHANA_SALADA_DE_FRUTA);
+        preSetup(plugin, Golem.SIMPLE_GOLEM);
+        preSetup(plugin, Golem.INTELLIGENCE_GOLEM);
+        preSetup(plugin, Golem.LOCK_GOLEM);
+        preSetup(plugin, Golem.BERSERK_GOLEM);
+        preSetup(plugin, Golem.EFFICIENTY_GOLEM);
+        preSetup(plugin, Golem.CLONING_GOLEM);
+        preSetup(plugin, Golem.ACCELERATION_GOLEM);
 
     }
 
