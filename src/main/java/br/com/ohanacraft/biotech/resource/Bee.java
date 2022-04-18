bc70267cd86fc5a8e9a785155a4131540db882bb
DoctorWil <wilian.lima.santoss@gmail.com>
2022-04-17 21:27:53 -0300

    textura da abelha
    textura da abelha

--- src/main/java/br/com/ohanacraft/biotech/resource/Bee.java
+++ src/main/java/br/com/ohanacraft/biotech/resource/Bee.java
@@ -9,37 +9,37 @@ public class Bee {
     public static final SlimefunItemStack SIMPLE_BEE =
             new SlimefunItemStack("BIOTECH_SIMPLE_BEE",
                     "259001a851bb1b9e9c05de5d5c68b1ea0dc8bd86babf188e0aded8f912c07d0d",
-                    "&eAbelha &fComum", "");
+                    "Abelha Comum", "");
 
     public static final SlimefunItemStack INTELLIGENCE_BEE =
             new SlimefunItemStack("BIOTECH_INTELLIGENCE_BEE",
                     "1445c3def689427fd8df8cad824c0a6b553d4f847eb03705196a69c7eed946af",
-                    "&eAbelha &fInteligente", "");
+                    "Abelha Inteligente", "");
 
     public static final SlimefunItemStack LUCK_BEE =
             new SlimefunItemStack("BIOTECH_LUCK_BEE",
                     "259001a851bb1b9e9c05de5d5c68b1ea0dc8bd86babf188e0aded8f912c07d0d",
-                    "&eAbelha &fSortuda", "");
+                    "Abelha Sortuda", "");
 
     public static final SlimefunItemStack BERSERK_BEE =
             new SlimefunItemStack("BIOTECH_BERSERK_BEE",
                     "d8916ea1ffb39f13dbbf4e62449dd2cbd5d2812f414e97608af0609e139d6115",
-                    "&eAbelha &fBerserk", "");
+                    "Abelha Berserk", "");
 
     public static final SlimefunItemStack EFFICIENCY_BEE =
             new SlimefunItemStack("BIOTECH_EFFICIENCY_BEE",
                     "fb7a761d715d2c3c73a7d01411e61f4237e4f7633cc9f4db2cf05f66d2030a17",
-                    "&eAbelha &fEficiente", "");
+                    "Abelha Eficiente", "");
 
     public static final SlimefunItemStack CLONING_BEE =
             new SlimefunItemStack("BIOTECH_CLONING_BEE",
                     "2ea2ba84c8c91dcb28af56a36dd49a1e51c3557e853f5d3777877758737b5557",
-                    "&eAbelha &fClonadora", "");
+                    "Abelha Clonadora", "");
 
     public static final SlimefunItemStack ACCELERATION_BEE =
             new SlimefunItemStack("BIOTECH_ACCELERATION_BEE",
-                    "ACCELERATION_BEE",
-                    "&eAbelha &fAceleradora", "");
+                    "725926d73f6f9480efad9e930e8a4a2566c115287a5b40ec0f50d58afab1a72a",
+                    "Abelha Aceleradora", "");
 
 
     public static void setup(BioTech plugin) {
--- src/main/java/br/com/ohanacraft/biotech/resource/IronGolem.java
+++ src/main/java/br/com/ohanacraft/biotech/resource/IronGolem.java
@@ -9,37 +9,37 @@ public class IronGolem {
     public static final SlimefunItemStack SIMPLE_GOLEM =
             new SlimefunItemStack("BIOTECH_SIMPLE_GOLEM",
                     "e13f34227283796bc017244cb46557d64bd562fa9dab0e12af5d23ad699cf697",
-                    "&eGolem &fComum", "");
+                    "Golem Comum", "");
 
     public static final SlimefunItemStack INTELLIGENCE_GOLEM =
             new SlimefunItemStack("BIOTECH_INTELLIGENCE_GOLEM",
                     "562f0e9a738265c75bdd92b9e2e02377fec9f3b027086a0dec273bbdf6798be",
-                    "&eGolem &fInteligente", "");
+                    "Golem Inteligente", "");
 
     public static final SlimefunItemStack LUCK_GOLEM =
             new SlimefunItemStack("BIOTECH_LOCK_GOLEM",
                     "ef6e7a9c941e71bbfeb9ccdf91119ed7868bef05e661b1e14d3e34a9b17088d9",
-                    "&eGolem &fSortuda", "");
+                    "Golem Sortudo", "");
 
     public static final SlimefunItemStack BERSERK_GOLEM =
             new SlimefunItemStack("BERSERK_GOLEM",
                     "ca14326aeac97f96189bb9969e7b955ec650d37b7c9790356ff09f32010f1ae0",
-                    "&eGolem &fBerserk", "");
+                    "Golem Berserk", "");
 
     public static final SlimefunItemStack EFFICIENCY_GOLEM =
             new SlimefunItemStack("EFFICIENCY_GOLEM",
                     "6d351a4ef45749565514d22f48e9695005a582b9ddbbbc444647ff0b056f4ffa",
-                    "&eGolem &fEficiente", "");
+                    "Golem Eficiente", "");
 
     public static final SlimefunItemStack CLONING_GOLEM =
             new SlimefunItemStack("CLONING_GOLEM",
                     "ec4a74ac2d0ceff89157ad16121b6bcb2b5e5e694b460a27d3b9944f368c17ce",
-                    "&eGolem &fClonadora", "");
+                    "Golem Clonador", "");
 
     public static final SlimefunItemStack ACCELERATION_GOLEM =
             new SlimefunItemStack("ACCELERATION_GOLEM",
                     "1bfb3534baf81dc3f82feffbcd1c9ae33fac7dcc8291d2ee43d819d5a9b61",
-                    "&eGolem &fAceleradora", "");
+                    "Golem Acelerador", "");
 
     public static void setup(BioTech plugin) {
 
